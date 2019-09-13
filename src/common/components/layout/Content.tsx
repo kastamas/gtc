4f2696b8d1d7182f5c5144445c31819f4e363d45
msidorov <mikxsid@gmail.com>
2019-09-13 13:10:31 +0300

    GTC-1 Init project

---
+++ src/common/components/layout/Content.tsx
@@ -0,0 +1,10 @@
+import { Layout } from 'antd';
+import React, { Component } from 'react';
+
+class ContentComponent extends Component {
+  render() {
+    return <Layout.Content>COntent</Layout.Content>;
+  }
+}
+
+export const Content = ContentComponent;
